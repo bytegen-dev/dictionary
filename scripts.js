1c777f8de18e542ba7c2d9f70a96f28357019459
tertiux44 <secondsonofadebayo@gmail.com>
2023-01-04 00:45:10 +0100

    nav

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
+const resultii = document.getElementById("resultii");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
@@ -14,18 +15,39 @@ btn.addEventListener("click", ()=>{
             <button onclick = "playSound()">
                 <i class="fa fa-volume-up"></i>
             </button>
-        </div>
-        <div class="details">
+            </div>
+            <div class="details">
             <p>${data[0].meanings[0].partOfSpeech}</p>
             <p>${data[0].phonetic}</p>
-        </div>
-        <p class="word-meaning">
+            </div>
+            <p class="word-meaning">
             ${data[0].meanings[0].definitions[0].definition}
-        </p>
-        <p class="word-example">
+            </p>
+            <p class="word-example">
             ${data[0].meanings[0].definitions[0].example || ""}
-        </p>`;
+            </p>`;
+
+            resultii.innerHTML = `<div class="word">
+            <h3>${inpWord}</h3>
+            <button onclick = "playSound()">
+                <i class="fa fa-volume-up"></i>
+            </button>
+            </div>
+            <div class="details">
+            <p>${data[0].meanings[1].partOfSpeech}</p>
+            <p>${data[0].phonetic}</p>
+            </div>
+            <p class="word-meaning">
+            ${data[0].meanings[1].definitions[0].definition}
+            </p>
+            <p class="word-example">
+            ${data[0].meanings[1].definitions[0].example || ""}
+            </p>`;
+
+
         sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+
+
         })
         .catch(() =>{
             result.innerHTML = `<h3 class="error" >Couldn't Find the Word</h3>`
