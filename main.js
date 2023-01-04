06ab75bc6a51bf7ea1a4ce9e4311dfd9cfab0dda
tertiux44 <secondsonofadebayo@gmail.com>
2023-01-04 01:13:10 +0100

    nav

---
+++ main.js
@@ -0,0 +1,19 @@
+// const results = document.querySelectorAll('.result')
+
+// const observer = new IntersectionObserver(entries => {
+    
+//     entries.forEach(entry => {
+//         entry.target.classList.toggle("show", entry.isIntersecting)
+//         if (entry.isIntersecting) observer.unobserve(entry.target)
+//     })
+
+// },
+// {
+//     threshold: 1,
+//     // rootMargin: "-50px"
+// }
+// )
+
+// results.forEach(result => {
+//     observer.observe(result)
+// })
--- scripts.js
+++ scripts.js
@@ -1,20 +1,24 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
+const resultHolder = document.getElementById("result-holder");
 const resultii = document.getElementById("resultii");
+const candy = document.getElementById("candy");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+const soundit = document.getElementById("soundit");
+var pagun = document.querySelector('.swiper-pagination');
 
 btn.addEventListener("click", ()=>{
+
     let inpWord = document.getElementById("inp-word").value;
     fetch(`${url}${inpWord}`)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
+            pagun.classList.add('showpagun')
+            soundit.classList.add('showsoundit')
+            candy.innerHTML = `${inpWord}`
             result.innerHTML = `<div class="word">
-            <h3>${inpWord}</h3>
-            <button onclick = "playSound()">
-                <i class="fa fa-volume-up"></i>
-            </button>
             </div>
             <div class="details">
             <p>${data[0].meanings[0].partOfSpeech}</p>
@@ -28,10 +32,6 @@ btn.addEventListener("click", ()=>{
             </p>`;
 
             resultii.innerHTML = `<div class="word">
-            <h3>${inpWord}</h3>
-            <button onclick = "playSound()">
-                <i class="fa fa-volume-up"></i>
-            </button>
             </div>
             <div class="details">
             <p>${data[0].meanings[1].partOfSpeech}</p>
@@ -50,6 +50,8 @@ btn.addEventListener("click", ()=>{
 
         })
         .catch(() =>{
+            soundit.classList.remove('showsoundit')
+            pagun.classList.remove('showpagun')
             result.innerHTML = `<h3 class="error" >Couldn't Find the Word</h3>`
         });
 });
@@ -65,3 +67,4 @@ var nav = document.querySelector(".nav");
 function dropmenu(){
     nav.classList.toggle('drop-menu')
 }
+
